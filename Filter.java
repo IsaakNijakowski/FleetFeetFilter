2bd9117f38b20591f337266c9b584b3c6970ac3d
Isaak Nijakowski <darkflaremc@gmail.com>
2022-11-17 19:34:46 -0500

    Implemented the programs function

--- Filter.java
+++ Filter.java
@@ -2,21 +2,35 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.PrintWriter;
 import java.util.Scanner;
-import Jsoup
+import java.util.ArrayList;
+import java.io.IOException;
+
+import org.jsoup.HttpStatusException;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
 
 class Filter {
-    public static void main(String[] args) throws FileNotFoundException {
-        System.out.println("Hello World");
+    public static void main(String[] args) throws FileNotFoundException, IOException, InterruptedException {
         File file = new File("./SKUs.txt");
         File output = new File("./Output.txt");
         PrintWriter write = new PrintWriter(output);
         Scanner scanner = new Scanner(file);
         write.write("");// Clear output contents
         while(scanner.hasNextLine()) {
-            System.out.println(scanner.nextLine());
+            ArrayList<Element> elements = new ArrayList<>();
+            String SKU = scanner.nextLine();
+            try {
+                Document site = Jsoup.connect("https://www.fleetfeet.com/browse?q="+SKU).get();
+                elements.addAll(site.getElementsByClass("no-results"));
+                if (elements.size()!=0) {
+                    write.append(SKU+"\n");
+                }
+            } catch(HttpStatusException e) {
+                System.out.println("Error on SKU "+SKU);
+            }
+            Thread.sleep(100);
         }
-        write.append("this is the real test");
-
         scanner.close();
         write.close();
     }
